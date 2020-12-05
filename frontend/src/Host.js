bd02f6441fcf3b5a6b052fb5415842bc4c3d7583
Zachary Sutrich <abocreature@gmail.com>
2020-12-05 13:36:46 -0500

    Finished Placeholders

--- frontend/src/Host.js
+++ frontend/src/Host.js
@@ -4,8 +4,42 @@ import {Link} from 'react-router-dom';
 export default class Host extends React.Component {
 	render() {
 		return (
-			<div className="homePage">
-				<h1>Placeholder</h1>
+			<div className="registerPage">
+				<div className="container">
+					<h4>Event Hosting</h4>
+					<form>
+						<input
+						 id="eventtitle"
+						 name="eventtitle"
+						 autoComplete="off"
+						 placeholder="Title"
+						/>
+						<p id="titleError" className="errorReturn" />
+						<textarea
+						 id="eventdesc"
+						 name="eventdesc"
+						 placeholder="Event Description"
+						/>
+						<input
+						 id="eventurl"
+						 name="eventurl"
+						 placeholder="Event URL"
+						/>
+						<p id="urlError" className="errorReturn" />
+						<div>
+							<button
+							 type="button"
+							 id="approveButton"
+							 type="submit">
+								Approve
+							</button>
+						</div>
+						<p
+						 id="errorReturn"
+						 className="errorReturn" 
+						/>
+					</form>
+				</div>
 			</div>
 		)
 	}
--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -4,8 +4,27 @@ import {Link} from 'react-router-dom';
 export default class Search extends React.Component {
 	render() {
 		return (
-			<div className="homePage">
-				<h1>Placeholder</h1>
+			<div className="userPage">
+				<div>
+					<div className="container">
+						<h4>Event Search</h4>
+						<form>
+							<input
+							 id="cityname"
+							 name="cityname"
+							 autoComplete="off"
+							 placeholder="City Name"
+							/>
+							<div>
+								<button
+								 type="button"
+								 type="submit">
+									Search
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
 			</div>
 		)
 	}
