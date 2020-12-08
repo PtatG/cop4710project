f42a4ddd922a8b415d52e5da32994da029abdf44
Zachary Sutrich <abocreature@gmail.com>
2020-12-08 16:03:50 -0500

    Lookup page progress

--- frontend/src/components/Lookup.js
+++ frontend/src/components/Lookup.js
@@ -1,16 +1,19 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default class Lookup extends React.Component {
+class Lookup extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			formdata: {
-				adminname: ""
+				adminname: "",
+				results: ""
 			}
 		}
 		this.changeForm = this.changeForm.bind(this);
-		this.submitForm = this.submitForm.bind(this);
+		this.submitUsers = this.submitUsers.bind(this);
+		this.submitAdmins = this.submitAdmins.bind(this);
 	}
 	
 	changeForm(e) {
@@ -21,68 +24,133 @@ export default class Lookup extends React.Component {
 			}
 		}))
 	}
-	
-	submitForm(e) {
+
+	submitUsers(e) {
 		console.log("Submitting...");
-		console.log(this.state.formdata)
+		
+		var jsonPayload = `
+			{
+				"token": "${this.props.user.token}"
+			}
+		`
+		
+		fetch('http://127.0.0.1:8080/listUsers/', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: jsonPayload,
+		}).then(res => {
+			console.log(res)
+			return res.json()
+		}).then(response => {
+			var elem = document.getElementById("search-err");
+			elem.innerHTML = response.message;
+			console.log('Success:', response);
+			this.setState({ results: response.users });
+			setTimeout(() => {
+				elem.innerHTML = "";
+			}, 5000)
+		});
+		
+		e.preventDefault();
+	}
+	
+	submitAdmins(e) {
+		console.log("Submitting...");
+		
+		var jsonPayload = `
+			{
+				"token": "${this.props.user.token}"
+			}
+		`
+		
+		fetch('http://127.0.0.1:8080/listAdmins/', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: jsonPayload,
+		}).then(res => {
+			console.log(res)
+			return res.json()
+		}).then(response => {
+			var elem = document.getElementById("search-err");
+			elem.innerHTML = response.message;
+			console.log('Success:', response);
+			this.setState({ results: response.admins });
+			setTimeout(() => {
+				elem.innerHTML = "";
+			}, 5000)
+		});
+		
 		e.preventDefault();
 	}
 	
 	render() {
 		const {formdata} = this.state;
+		
+		const sresult = []
+		const temp = []
+		
+		if (this.state.results != null)
+		{
+			temp.push(
+				<div>
+					<tbody>
+						<tr>
+							<td>
+								Username
+							</td>
+						</tr>
+					</tbody>
+				</div>
+			)
+			for (var i = 0; i < this.state.results.length; i++)
+			{
+				temp.push(
+					<div>
+						<tbody key={i}>
+							<tr id={i} key={i}>
+								<td>
+									{this.state.results[i].username}
+								</td>
+							</tr>
+						</tbody>
+					</div>
+				)
+			}
+			sresult.push(
+				<table className="resultsTable">
+					{temp}
+				</table>
+			)
+		}
 		return (
 			<div className="userPage">
-				<div className="container">
-					<h4>Host Search</h4>
-					<form onSubmit={this.submitForm}>
-						<input
-						 id="adminname"
-						 name="adminname"
-						 autoComplete="off"
-						 placeholder="Search Term"
-						 onChange={this.changeForm}
-						 value={formdata.adminname}
-						/>
-						<div>
-							<button type="submit">
-								Search Hosts
-							</button>
-						</div>
+				<div className="scontainer">			
+					<p id="search-err" className="errorReturn"></p>
+					<form onSubmit={this.submitUsers}>
+						<button type="submit">
+							All Users
+						</button>
 					</form>
-					<button type="submit">
-						All Users
-					</button>
-					<button type="submit">
-						All Admins
-					</button>
-					<form onSubmit={this.submitForm}>
-						<input
-						 id="adminname"
-						 name="adminname"
-						 autoComplete="off"
-						 placeholder="Search Term"
-						 onChange={this.changeForm}
-						 value={formdata.adminname}
-						/>
-						<div>
-							<button type="submit">
-								Search Admins
-							</button>
-						</div>
+					<form onSubmit={this.submitAdmins}>
+						<button type="submit">
+							All Admins
+						</button>
 					</form>
 				</div>
 				<div className="container">
-					<button 
-					 className="collapsible">
-						Example Result
-					</button>
-					<div className="content">
-						<p>Event 1</p>
-						<p>Event 2</p>
-						<p>Event 3</p>
-					</div>
+					{sresult}
 				</div>
 			</div>
 		)
 	}
 }
+
+const mapState = state => ({
+	user: state.user
+})
+
+export default connect(mapState)(Lookup);
